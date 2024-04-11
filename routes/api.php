36873f38e3ec5018630a0fb07be582a7169a2abe
Chriford Siame <143299729+chriford-siame@users.noreply.github.com>
2024-04-11 20:48:53 +0200

    Implement APIs (#2)
    * create a model, create a migration
    
    * update migration
    
    * create a post factory
    
    * modify the db seeder
    
    * create the post model
    
    * create composer files
    
    * create controller
    
    * create apis
    
    * modify the web apis
    
    * modify the post factory
    
    ---------
    
    Co-authored-by: Chriford Siame <chrifordsiame@booknowzambia.com>

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {return "Hello world";})->name('posts_view');
+Route::get('/posts', [PostController::class,'get_posts'])->name('posts_view');
+Route::get('/posts/{post}/get', [PostController::class,'get_post'])->name('post_view');
+Route::put('/posts/{post}/update', [PostController::class,'update_post'])->name('update_view');
+Route::delete('/posts/{post}/delete', [PostController::class,'delete_post'])->name('delete_view');
+Route::post('/post/create', [PostController::class,'create_post'])->name('create_view');
